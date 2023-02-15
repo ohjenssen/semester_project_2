03bf44e3b5cc22a313a3ad062b0178764ef452d5
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-15 21:35:10 +0100

    updated route

--- src/js/routes.mjs
+++ src/js/routes.mjs
@@ -1,10 +1,16 @@
-import * as listeners from "./handlers/index.mjs";
+import * as listeners from "./listeners/index.mjs";
 
 export function setRoute(path){
     switch(path){
         case "/pages/register/":
             listeners.setRegisterFormListener();
             break;
+
+        case "/pages/login/":
+            listeners.setLoginFormListener();
+            break;
     }
+
+    listeners.checkLoginStatus();
 
 }
