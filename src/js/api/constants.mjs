38295f5998688e77e8bd6963fc1d8109fa14d753
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-15 21:35:23 +0100

    added login url

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -1,4 +1,5 @@
 export const api_urls = {
     base: "https://nf-api.onrender.com",
-    register: "/api/v1/auction/auth/register"
+    register: "/api/v1/auction/auth/register",
+    login: "/api/v1/auction/auth/login"
 }
