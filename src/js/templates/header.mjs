19c0c6ad26770a76f8d9410a6d95253cd83ecb76
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-16 12:38:16 +0100

    Updated id

--- src/js/templates/header.mjs
+++ src/js/templates/header.mjs
@@ -24,6 +24,7 @@ export function logoutBtn(){
     logoutBtn.className = "btn btn-outline-light me-2";
     logoutBtn.href = "/";
     logoutBtn.innerText = "LOGOUT";
+    logoutBtn.id = "logoutBtn";
 
     return logoutBtn;
 }
@@ -33,6 +34,7 @@ export function userProfileAvatar(){
     aTag.className = "me-2";
     aTag.innerHTML = "<img>";
     aTag.href = "";
+    aTag.id = "profileAvatar";
 
     aTag.querySelector("img").alt = "profile image";
     aTag.querySelector("img").className = "img-fluid rounded-circle border border-dark border-3";
