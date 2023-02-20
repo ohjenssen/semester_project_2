d1f3044fc7a67ef9839f964341c0678a15cca618
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-20 17:50:04 +0100

    get profile link

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -1,5 +1,6 @@
 export const api_urls = {
     base: "https://nf-api.onrender.com",
     register: "/api/v1/auction/auth/register",
-    login: "/api/v1/auction/auth/login"
+    login: "/api/v1/auction/auth/login",
+    profile: "/api/v1/auction/profiles/"
 }
