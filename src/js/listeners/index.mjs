2130b9cb91d2bf2763ad5ec2cd11771dd5c6e3af
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-20 12:04:48 +0100

    routes updated

--- src/js/listeners/index.mjs
+++ src/js/listeners/index.mjs
@@ -2,3 +2,5 @@ export { setRegisterFormListener } from "./register.mjs";
 export { setLoginFormListener } from "./login.mjs";
 export { checkLoginStatus } from "./loginStatus.mjs";
 export { logout } from "./logout.mjs";
+export { scrollFunction } from "./scrollBtn.mjs";
+export { setLandingPage } from "./landingPage.mjs";
--- src/js/routes.mjs
+++ src/js/routes.mjs
@@ -9,6 +9,11 @@ export function setRoute(path){
         case "/pages/login/":
             listeners.setLoginFormListener();
             break;
+
+        case "/":
+            listeners.setLandingPage();
+            window.onscroll = function() {listeners.scrollFunction()};
+            break;
     }
 
     listeners.checkLoginStatus();
