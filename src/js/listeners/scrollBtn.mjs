4195acc346d947312249dda5084e2c465522d17d
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-20 12:04:38 +0100

    Scroll button conditons

---
+++ src/js/listeners/scrollBtn.mjs
@@ -0,0 +1,21 @@
+import * as localStorage from "../localStorage/index.mjs";
+
+export function scrollFunction() {
+    let scrollBtn = document.getElementById("scrollBtn");
+
+            if(localStorage.getAccessToken()){
+                scrollBtn.href = "/pages/browse";
+                scrollBtn.innerText = "Create listing";
+            } else {
+                scrollBtn.href = "/pages/register/";
+                scrollBtn.innertext = "Create User";
+            }
+
+    const screenWidth = screen.width;
+
+    if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && screenWidth > 480) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
