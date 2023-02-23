eefa1cd10c5e3cd47c40f531f704dc130b9b3cbd
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-23 21:03:41 +0100

    Added conditions to rendering and deletebtn

--- src/js/templates/posts.mjs
+++ src/js/templates/posts.mjs
@@ -1,47 +1,56 @@
-const divContainer = document.createElement("div");
+import { deleteModal } from "./modal.mjs";
 
-export function userProfilePosts(listings){
+export function userProfilePosts(listing){
+    
+    const divContainer = document.createElement("div");
     divContainer.className = "card bg-dark-blue text-light-blue shadow mt-4 card-listing";
 
     divContainer.innerHTML = `
-                                <div class="image-container bg-light-blue rounded-top">
-                                    <img src="" class="card-img-top mx-auto d-block card-image" alt="Listing image">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <div class="d-flex justify-content-between flex-wrap mb-1">
-                                        <div class="me-3 mt-2">
-                                            <p class="card-text mb-0">Deadline</p>
-                                            <p id="deadline" class="card-text fs-5"></p>
+                                    <div class="image-container bg-light-blue rounded-top">
+                                        <img src="" class="card-img-top mx-auto d-block card-image" alt="Listing image">
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <div class="d-flex justify-content-between flex-wrap mb-1">
+                                            <div class="me-3 mt-2">
+                                                <p class="card-text mb-0">Deadline</p>
+                                                <p id="deadline" class="card-text fs-5"></p>
+                                            </div>
+                                            <div class="mt-2">
+                                                <p class="card-text mb-0">Bids</p>
+                                                <p id="bids" class="card-text fs-5"></p>
+                                            </div>
                                         </div>
-                                        <div class="mt-2">
-                                            <p class="card-text mb-0">Bids</p>
-                                            <p id="bids" class="card-text fs-5"></p>
+                                        <div>
+                                        <a id="view" class="btn btn-light-blue shadow mt-2">View</a>
+                                        <a id="edit" href="#" class="btn btn-orange shadow mt-2">Edit</a>
+                                        <button type="button" class="btn btn-danger text-dark-blue shadow mt-2 float-end" data-bs-toggle="modal" data-bs-target="#deleteModal${listing.id}">Delete</a>
                                         </div>
                                     </div>
-                                        <a id="view" href="#" class="btn btn-light-blue shadow mt-2">View</a>
-                                        <a id="edit" href="#" class="btn btn-orange shadow mt-2">Edit</a>
-                                </div>
-                                
-    `;
+                                    
+                                `;
 
-    if(listings < 1){
-        divContainer.innerText = "No listings";
-        divContainer.className = "text-center fs-2";
+
+    divContainer.querySelector(".card-title").innerText = listing.title;
+
+    if(listing.media[0]){
+        divContainer.querySelector("img").src = listing.media[0];
     } else {
-        listings.map((listing, index) => {
-            divContainer.querySelector(".card-title").innerText = listing.title;
-            divContainer.querySelector("img").src = listing.media[0];
-            const d = new Date(listing.endsAt);
-            divContainer.querySelector("#deadline").innerText = d.toLocaleString();
-            divContainer.querySelector("#bids").innerText = listing._count.bids;
-            divContainer.querySelector("#view").href = `/pages/listing/?id=${listing.id}`;
-        })
-    }
+        divContainer.querySelector("img").src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930";
+    };
+    
+    const d = new Date(listing.endsAt);
+    divContainer.querySelector("#deadline").innerText = d.toLocaleString();
+    divContainer.querySelector("#bids").innerText = listing._count.bids;
+    divContainer.querySelector("#view").href = `/pages/listing/?id=${listing.id}`;
+
+    divContainer.append(deleteModal(listing.id));
+
     return divContainer;
 }
 
 export function singeListing(listing){
+    const divContainer = document.createElement("div");
     divContainer.className = "text-dark-blue mt-4";
 
     divContainer.innerHTML = `
@@ -111,13 +120,24 @@ export function singeListing(listing){
     const d = new Date(listing.endsAt);
     divContainer.querySelector("#deadline").innerText = d.toLocaleString();
 
-    if(listing.tags){
-        divContainer.querySelector("#tags").innerText = listing.tags;
-    } else {
+    if(listing.tags[0] === ""){
         divContainer.querySelector("#tags").innerText = "No tags";
+    } else {
+        divContainer.querySelector("#tags").innerText = listing.tags;
     }
     // divContainer.querySelector("#bids").innerText = listing._count.bids;
-    // divContainer.querySelector("#view").href = `/pages/listing/?id=${listing.id}`;
+    // divContainer.querySelector("#view").href = `/pages/listing/?id=${listing.id}`; 
 
     return divContainer;
 }
+
+export function noListings(){
+    const divContainer = document.createElement("h1");
+    divContainer.className = "card bg-dark-blue text-light-blue shadow mt-4 card-listing p-3 text-center";
+
+    divContainer.innerText = "No listings yet!";
+    return divContainer;
+}
+
+// "#deleteModalb73aef1e-56c3-41fd-87d2-ea2e1be550ae"
+// "#deleteModalb73aef1e-56c3-41fd-87d2-ea2e1be550ae"
