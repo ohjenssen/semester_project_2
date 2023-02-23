14be23243f2edef9f3084599c379946d28e89d7e
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-23 21:04:29 +0100

    added create listing functionality

---
+++ src/js/listeners/create/createListing.mjs
@@ -0,0 +1,27 @@
+import * as api from "../../api/index.mjs";
+export function createListing(){
+    const form = document.querySelector("#createListing");
+
+    if(form){
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.target);
+            const listingInfo = Object.fromEntries(formData.entries());
+
+            const rawDate = `${listingInfo.date}-${listingInfo.time}`;
+            const deadline = rawDate.replace(/(\d{4})-(\d{2})-(\d{2})-(\d{2}):(\d{2})/, '$1-$2-$3T$4:$5:00.000Z');
+            const { date, time, ...data } = listingInfo;
+
+            const tagsArr = data.tags.split(" ");
+            const mediaArr = data.media.split("")
+
+            data.endsAt = deadline;
+            data.media = mediaArr;
+            data.tags = tagsArr;
+            console.log(data)
+            api.createListing(data);
+
+            
+        })
+    }
+}
