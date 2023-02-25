2665564668c6674e8d7678c7ac569e08eef6a911
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-25 19:43:03 +0100

    removed consolelogging

--- src/js/listeners/read/getSingleListing.mjs
+++ src/js/listeners/read/getSingleListing.mjs
@@ -4,7 +4,6 @@ import * as templates from "../../templates/index.mjs";
 export async function getSingleListing(){
     const div = document.querySelector("#listings");
     const listing = await api.getSingleListing();
-    console.log(listing)
     
     
     div.append(templates.singeListing(listing));
