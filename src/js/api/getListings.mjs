30b32f1b4d6b31225864da71551ca4d7f3d3071a
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-27 10:58:16 +0100

    Rotuing

--- src/js/api/getListings.mjs
+++ src/js/api/getListings.mjs
@@ -8,16 +8,20 @@ const data = {
         "Content-type": "application/json; charset=UTF-8"
 }}
 
-export async function getListings(id, profileName){
+export async function getListings(profileName){
     let url = api_urls.base;
     const path = location.pathname;
     
     switch (path){
         case "/":
-            url += `${api_urls.listings}`;
+            url += `${api_urls.listings}?_active=true`;
             break;
 
         case "/pages/profilePage/":
+            url += `${api_urls.profile}${profileName}/listings`;
+            break;
+
+        case "/pages/otherProfiles/":
             url += `${api_urls.profile}${profileName}/listings`;
             break;
     }
