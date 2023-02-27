b2d2b8295584f6053196e00e944a25f144b2b29f
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-27 11:06:23 +0100

    routing

--- src/js/templates/index.mjs
+++ src/js/templates/index.mjs
@@ -1,5 +1,5 @@
 export { loginBtn, registerBtn, userProfileAvatar,  logoutBtn} from "./header.mjs";
 export { loggedIn, notLoggedIn } from "./landingpage.mjs";
 export { renderProfile } from "./profilePage.mjs";
-export { userProfilePosts, singeListing, noListings } from "./posts.mjs";
+export { browseListings, userProfilePosts, singeListing, noListings } from "./posts.mjs";
 export { preFillForm } from "./editListing.mjs";
