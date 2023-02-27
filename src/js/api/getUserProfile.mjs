555c471767746eb946be7995c8a333d0bfe3d7cc
ohjenssen <oskar_jenssen@hotmail.com>
2023-02-27 10:58:47 +0100

    changed some logic in code

--- src/js/api/getUserProfile.mjs
+++ src/js/api/getUserProfile.mjs
@@ -1,14 +1,19 @@
 import { api_urls } from "./constants.mjs";
-import * as localStorage from "../localStorage/index.mjs";
+import * as storage from "../localStorage/index.mjs";
 
 export async function getUserProfile(){
-    const profileUrl = `${api_urls.base}${api_urls.profile}${localStorage.getUserName()}?_listings=true`;
-    const accessToken = localStorage.getAccessToken();
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+    const name = params.get("name");
+
+
+    // const profileUrl = `${api_urls.base}${api_urls.profile}${storage.getUserName()}?_listings=true`;
+    const profileUrl = `${api_urls.base}${api_urls.profile}${name}?_listings=true`;
 
     const response = await fetch(profileUrl, {
         method: "GET",
         headers: {
-            "Authorization": `Bearer ${accessToken}`,
+            "Authorization": `Bearer ${storage.getAccessToken()}`,
             "Content-type": "application/json; charset=UTF-8"
         }
     })
