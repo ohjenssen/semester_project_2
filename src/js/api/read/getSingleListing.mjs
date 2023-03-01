3b3bde67e3bee7d1abd8b9b3d6ca195dde170618
ohjenssen <oskar_jenssen@hotmail.com>
2023-03-01 11:24:50 +0100

    minor fixes

--- src/js/api/read/getSingleListing.mjs
+++ src/js/api/read/getSingleListing.mjs
@@ -16,6 +16,6 @@ export async function getSingleListing(){
     const url =`${api_urls.base}${api_urls.listings}/${id}?_seller=true&_bids=true`;
 
     const response = await fetch(url, data);
-    const json = response.json();
+    const json = await response.json();
     return json;
 };
