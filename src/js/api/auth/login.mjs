cb6a9e3368392b7ab179c37eeef20b1cffd1b502
ohjenssen <oskar_jenssen@hotmail.com>
2023-03-02 20:57:35 +0100

    fixed a login issue

--- src/js/api/auth/login.mjs
+++ src/js/api/auth/login.mjs
@@ -6,28 +6,31 @@ export async function loginUser(profileData){
     const loginUrl = api_urls.base + api_urls.login;
     const loginBtn = document.querySelector("#loginBtn");
 
-    const response = await fetch(loginUrl, {
-        method: "POST",
-        body: JSON.stringify(profileData),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-
-    const json = await response.json();
     loginBtn.innerHTML = "";
     loginBtn.append(templates.addLoader());
-
     
-    if(response.ok){
-        localStorage.setItem("profileInfo", JSON.stringify(json));
-        localStorage.setItem("accessToken", JSON.stringify(json.accessToken));
-        localStorage.setItem("avatar", JSON.stringify(json.avatar));
-        window.location.replace("/");
-    } else if(!response.ok){
-        loginBtn.innerHTML = "LOGIN";
-        const errorMessage = await json.errors[0].message;
-        const pTag = document.querySelector("#errorTag");
-        pTag.innerHTML = errorMessage;
+    try {
+            const response = await fetch(loginUrl, {
+                method: "POST",
+                body: JSON.stringify(profileData),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            const json = await response.json();
+            
+            if(response.ok){
+                localStorage.setItem("profileInfo", JSON.stringify(json));
+                localStorage.setItem("accessToken", JSON.stringify(json.accessToken));
+                localStorage.setItem("avatar", JSON.stringify(json.avatar));
+                window.location.replace("/");
+            } else if(!response.ok){
+                loginBtn.innerHTML = "LOGIN";
+                const errorMessage = await json.errors[0].message;
+                const pTag = document.querySelector("#errorTag");
+                pTag.innerHTML = errorMessage;
+            }
+    } catch(error) {
+        console.log(error);
     }
 }
